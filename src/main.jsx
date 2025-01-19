9f1cfbbaf4d4bf22beec91a858f1e92ef5a3a324
Raidah Gauri <raidahgauri@yahoo.com>
2025-01-19 19:03:10 +0000

    Added environment variable support

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import PricingPage from './landing_page/pricing/PricingPage.jsx';
 import SupportPage from './landing_page/support/SupportPage.jsx';
 import Navbar from './landing_page/Navbar.jsx';
 import Footer from './landing_page/Footer.jsx';
-import NotFound from './landing_page/notFound.jsx';
+import NotFound from './landing_page/NotFound.jsx';
 
 createRoot(document.getElementById('root')).render(
  <BrowserRouter>
